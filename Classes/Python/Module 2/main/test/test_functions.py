0076683f41309bff73675390dd8c6ec360d9912e
Evan <evan9633@gmail.com>
2020-06-03 16:01:29 -0500

    updated tests

---
+++ Classes/Python/Module 2/main/test/test_functions.py
@@ -0,0 +1,20 @@
+import unittest
+from main import camper_age_input
+
+
+class FunctionTestCase(unittest.Testcase):
+    def test_something(self):
+        self.assertEqual(False,True)
+
+if __name__ == '_main__':
+    unittest.main()
+
+def test_age():
+    assertEquals(convert_to_months(2),24)
+    assertEquals(convert_to_months(5),60)
+    assertFalse(convert_to_months(-2),24)
+    assertRaises(convert_to_months("string")
+
+
+
+
